f18afcfa6aeffd90f89204f33771f3123704447f
tsuki <p.ponhchareon@gmail.com>
2024-12-15 15:55:11 +0700

    Add: setcookie

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { UsersService } from './users.service';
 import { User } from './interfaces/user.interface';
 import { LoginDto } from './dto/login.dto';
@@ -20,8 +29,15 @@ export class UsersController {
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
+  async login(@Body() loginDto: LoginDto, @Res() res: Response) {
     const user = await this.usersService.validateUser(loginDto);
-    return this.usersService.login(user);
+    const result = await this.usersService.login(user);
+    res.cookie('token', result.access_token, {
+      httpOnly: true,
+      secure: false, // true ถ้าใช้ HTTPS
+      sameSite: 'none', // หรือ 'none' สำหรับ cross-origin
+      maxAge: 7200000, // อายุ cookie 1 ชั่วโมง
+    });
+    res.json(result);
   }
 }
